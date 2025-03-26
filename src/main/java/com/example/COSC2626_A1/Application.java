be6613e5c1bc8910be8dbfa345fb18721558bd79
Marty-L <m.c.lawrence89@gmail.com>
2025-03-26 11:44:16 +1100

    Started Task 2 - application successfully parsing JSON - Added Jackson dependency to pom.xml - Moved /data folder to src/main/resources/ - Added JsonParserService to read JSON file - Temporarily changed port to 8081 for local testing

--- src/main/java/com/example/COSC2626_A1/Application.java
+++ src/main/java/com/example/COSC2626_A1/Application.java
@@ -1,13 +1,30 @@
 package com.example.COSC2626_A1;
 
+import com.example.COSC2626_A1.service.JsonParserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Application {
+import java.util.List;
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	@Autowired
+	private final JsonParserService jsonParserService;
+
+	public Application(JsonParserService jsonParserService) {
+		this.jsonParserService = jsonParserService;
 	}
 
+	//RUN THE MAIN SERVER APPLICATION
+	public static void main(String[] args) {SpringApplication.run(Application.class, args);}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//TODO: Fix file location
+		List<String> imageURLs = jsonParserService.extractImageURLs("C:/Users/mclaw/IdeaProjects/COSC2626-A1/src/main/resources/data/2025a1.json");
+	}
 }
---
+++ src/main/java/com/example/COSC2626_A1/service/JsonParserService.java
@@ -0,0 +1,49 @@
+package com.example.COSC2626_A1.service;
+
+import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class JsonParserService {
+
+    //Object mapper for reading the JSON tree
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public List<String> extractImageURLs(String filePath) throws IllegalArgumentException {
+        //Extract the image URLs from the JSON at filePath and return as a List.
+        //Will throw IllegalArgumentException if "songs" isn't a JSON array
+        //Adapted from example here : https://mkyong.com/java/jackson-how-to-parse-json/#parse-json-array-with-jackson
+        //Viewed: 2025-03-26
+
+        List<String> imageURLs = new ArrayList<>();
+
+        try {
+            //Get the JSON root and the songs array
+            //TODO: Really should be checking root and songsArray for null here...
+            JsonNode rootNode = objectMapper.readTree(new File(filePath));
+            JsonNode songsArray = rootNode.get("songs");
+
+            //Iterate through the "songs" array and grab the image URLs, adding to the list
+            if (songsArray.isArray()) {
+                for (JsonNode songNode : songsArray) {
+                    String imageURL = songNode.get("img_url").asText();
+                    System.out.println("Got URL:" + imageURL); //TODO: Remove this debug print
+                    imageURLs.add(imageURL);
+                }
+            } else throw new IllegalArgumentException("ERROR (parsing JSON): \"songs\" array is not an array");
+
+        } catch (IOException e) {
+            //TODO: Probably need better Exception handling than this
+            e.printStackTrace();
+        }
+
+        return imageURLs;
+    }
+}
