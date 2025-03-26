4c9115d28db88f28670abe6ad9205a6ee4c5a450
Bob Qian <s3840234@student.rmit.edu.au>
2025-03-26 13:55:11 +1100

    Updated application to use dynamic fie path

--- src/main/java/com/example/COSC2626_A1/Application.java
+++ src/main/java/com/example/COSC2626_A1/Application.java
@@ -2,11 +2,17 @@ package com.example.COSC2626_A1;
 
 import com.example.COSC2626_A1.service.JsonParserService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @SpringBootApplication
@@ -14,6 +20,9 @@ public class Application implements CommandLineRunner {
 
 	@Autowired
 	private final JsonParserService jsonParserService;
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class.getName());
+
 
 	public Application(JsonParserService jsonParserService) {
 		this.jsonParserService = jsonParserService;
@@ -25,6 +34,13 @@ public class Application implements CommandLineRunner {
 	@Override
 	public void run(String... args) {
 		//TODO: Fix file location (https://www.baeldung.com/spring-classpath-file-access)
-		List<String> imageURLs = jsonParserService.extractImageURLs("C:/Users/mclaw/IdeaProjects/COSC2626-A1/src/main/resources/data/2025a1.json");
+		try {
+			File file = new ClassPathResource("data/2025a1.json").getFile();
+			String filePath = file.getAbsolutePath();
+			List<String> imageURLs = jsonParserService.extractImageURLs(filePath);
+		} catch (IOException e) {
+			LOGGER.error("Error reading file: " + e.getMessage());
+		}
+
 	}
 }
